99a154d5dbac2bd8a1a6df8b195186ba3dcaae9a
nolageek <nolageek@gmail.com>
2016-04-28 23:09:28 -0400

    Modified default settings.js
    Still not sure how I'm going to package the default values. Just
    starting to think about how to do this for public consumption as opposed
    to just using it for myself.

--- text/menu/mystique/settings.js
+++ text/menu/mystique/settings.js
@@ -1,17 +1,19 @@
 var color = {
-txt_menu	: '\1h\1b', // Menu name
-txt_user 	: '\1h\1m', // username in menus
-txt_sym 	: '\1h\1k', // symbols color @, [ ], ( ),etc..
-txt_sym2 	: '\1h\1w', // symbol highlght (numbers in [1], menu options [A], etc..)
-txt_text 	: '\1h\1b', // color for most text
-txt_text2 	: '\1h\1m', // aux color for text, bold words, values, etc...
-txt_ques 	: '\1h\1c', // color for question prompts
-txt_alert 	: '\1h\1r', // color for alert text
-txt_success : '\1h\1g', // color for success text
-txt_info 	: '\1h\1c', // color for info text 
-def_text 	: '\1h\1b', // defaults option text
-def_value 	: '\1h\1c', // defaults current value
-def_on 		: '\1h\1c', // defaults 'on' color
-def_off 	: '\1n\1c', // defaults 'off' color
-def_head 	: '\1h\1w'  // defaults header
+# Uncomment and change the value of any item to overide default value
+	
+#txt_menu	: '\1h\1b', // Menu name
+#txt_user 	: '\1h\1c', // username in menus 
+#txt_sym 	: '\1n\1b', // symbols color @, [ ], ( ),etc..
+#txt_sym2 	: '\1h\1c', // symbol highlght (numbers in [1], menu options [A], etc..)
+#txt_text 	: '\1h\1b', // color for most text
+#txt_text2 	: '\1h\1m', // aux color for text, bold words, values, etc...
+#txt_ques 	: '\1h\1y', // color for question prompts
+#txt_alert 	: '\1h\1r', // color for alert text
+#txt_success : '\1h\1g', // color for success text
+#txt_info 	: '\1h\1c', // color for info text 
+#def_text 	: '\1h\1b', // defaults option text
+#def_value 	: '\1h\1c', // defaults current value
+#def_on 	: '\1n\1c', // defaults 'on' color
+#def_off 	: '\1n\1c', // defaults 'off' color
+#def_head 	: '\1h\1w'  // defaults header
 }
